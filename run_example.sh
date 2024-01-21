a10fb59c7265bb97be5cc435bdeb0d783fde6720
sergeicu <serge.vasylechko@childrens.harvard.edu>
2024-01-21 17:39:54 -0500

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -2,7 +2,18 @@
 export REPLICATE_API_TOKEN=<>
 source <your_venv or conda>
 
-# run model 
+# download model .zip files 
+
+
+
+# run model - together 
 model=lucataco/sdxl-lcm
-zip_or_folder=fonts-1.zip
+zip_or_folder=fonts-2.zip
 python tune_folder_any_replicate_model.py $zip_or_folder $model
+# you will be prompted for model name - give - ltejedor/sundai-fonts-letters-together
+
+# run model - separate
+model=lucataco/sdxl-lcm
+zip_or_folder=fonts-3.zip
+python tune_folder_any_replicate_model.py $zip_or_folder $model
+# you will be prompted for model name - give - ltejedor/sundai-fonts-letters-separate
